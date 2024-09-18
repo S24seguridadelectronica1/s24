c6d04e56ffc7f37a4e48df9c2cd42a90008cad32
fernando <s24h@outlook.es>
2024-09-17 20:52:12 -0500

    Actualizada la ruta de la imagen para GitHub Pages

--- src/GymImage.js
+++ src/GymImage.js
@@ -4,8 +4,8 @@ const GymImage = () => {
   return (
     <div className="my-8">
       <img 
-        src="/logo192.png" 
-        alt="FBT Functional Body Training Gym" 
+        src={process.env.PUBLIC_URL + '/logo192.png'} 
+        alt="FBT Functional Body Training Gym Logo" 
         className="w-full h-auto rounded-lg shadow-lg"
       />
     </div>
