81b5f691e6bd50564c3288d69856b6d3d52032d6
fernando <s24h@outlook.es>
2024-11-18 03:49:27 -0500

    Updates

--- static/js/952.2df294a6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpagina_web_s24=self.webpackChunkpagina_web_s24||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.2df294a6.chunk.js.map
